d5dff37bf6cd971483ba9a42f7cfeae06addd3bb
Tania Manoli <tania@atypicalrobot.com>
2017-04-16 20:49:16 +0000

    Add studytime.subjects app to INSTALLED_APPS in settings.

--- config/settings/base.py
+++ config/settings/base.py
@@ -59,6 +59,7 @@ LOCAL_APPS = [
     # custom users app
     'studytime.users.apps.UsersConfig',
     # Your stuff: custom apps go here
+    'studytime.subjects',
 ]
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
