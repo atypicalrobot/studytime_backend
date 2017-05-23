5cda78c9f0a09494261cf1ff70d796678634077b
atypicalrobot <github@atypicalrobot.com>
2017-05-23 03:43:26 +0000

    Fix vectors and fix django rest framework session authentication.

--- config/settings/base.py
+++ config/settings/base.py
@@ -307,6 +307,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'oauth2_provider.ext.rest_framework.OAuth2Authentication',
+        'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_FILTER_BACKENDS': (
         'rest_framework_filters.backends.DjangoFilterBackend',
